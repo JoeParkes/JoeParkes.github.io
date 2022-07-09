18cdde8006ea4f06d2436492d5d99736fa5960be
Joe Parkes <j.parkes@loungeunderwear.com>
2022-07-09 16:35:35 +0100

    Adding in header component

---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <section id="header">
+        <div>
+            <p>Joe</p>
+        </div>
+    </section>
+  );
+}
